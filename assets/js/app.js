90b4c09fbdbc23818f5c03c2cf2acf7dcbd00127
goddentalk <goddentalk@gmail.com>
2022-11-03 14:15:18 +0300

    OKBook

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,4 +34,17 @@ $(function() {
 
     });
 
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event){
+        
+        event.preventDefault();
+
+        var blockId= $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+
 })
